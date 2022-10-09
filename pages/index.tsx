fe7a1247bca14fd0bf6f5e21475b39ded7d01edb
chandrakant5 <chandrakant@spliceglobal.com>
2022-10-10 02:13:31 +0530

    updaste

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ interface Props{
   collections:Collection[]
 }
 const Home = ({collections}:Props) => {
-  console.log(collections,"788");
+
   return (
     <div className=" mx-auto flex min-h-screen max-w-7xl flex-col py-20 px-10 2xl:px-0">
       <Head>
@@ -75,7 +75,7 @@ export const getServerSideProps:GetServerSideProps =async () => {
 
   }`
   const collections = await sanityClient.fetch(query);
-  console.log(collections)
+
   return {
     props:{
       collections
--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,22 +1,101 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import {sanityClient,urlFor} from "../../sanity"
 import type { NextPage,GetServerSideProps } from 'next'
-import {useAddress,useDisconnect,useMetamask} from "@thirdweb-dev/react"
+import {useAddress,useDisconnect,useContract,useNFTDrop , useMetamask} from "@thirdweb-dev/react"
 import { Collection } from '../../typing'
 import Link from 'next/link'
+import { BigNumber } from 'ethers'
+import toast,{Toaster} from "react-hot-toast"
+
 interface Props{
   collection:Collection
 }
 function NFTDropPage({collection}:Props) {
-  
-    const connectWithMetamask =useMetamask();
+  const [clamiedsuplly,setclamiedsuplly] = useState<number>(0);
+  const [totalsuplly,settotalsuplly] = useState<BigNumber>();
+  const [loading,setloading] = useState<boolean>(true);
+  const[priceInEth,setPriceInEth] = useState<string>();
+  const nftDrop = useNFTDrop(collection.address);
+ 
+
+useEffect(()=>{
+  if(!nftDrop) return;
+  const fetchPrices=async()=>{
+    const clamiedConditions = await  nftDrop.claimConditions.getAll()
+    setPriceInEth(clamiedConditions?.[0].currencyMetadata.displayValue)
+  }
+  fetchPrices();
+},[nftDrop])
+  useEffect(()=>{
+    if(!nftDrop) return;
+    const fetchNFTDropData=async()=>{
+      setloading(true)
+      const claimed=await nftDrop.getAllClaimed();
+      const total=await nftDrop.totalSupply();
+      setclamiedsuplly(claimed.length)
+      settotalsuplly(total)
+      setloading(false)
+    }
+    fetchNFTDropData()
+  },[nftDrop])  
+  const minNFT=()=>{
+    setloading(true)
+    const notification= toast.loading("Minting...",{
+      style:{
+        background:"#fff",
+        color:"green",
+        fontWeight:"bolder",
+        fontSize:"17px",
+        padding:"20px"
+
+      }
+    })
+   if(!nftDrop || !address) return;
+   const quantity=1
+   nftDrop.claimTo(address,quantity).then(async(tx)=>{
+    const receipt=tx[0].receipt
+    const clamiedTokenId=tx[0].id
+    const claimedNFT= await tx[0].data()
+    toast("YUPP ..You sucessfully implemented",{
+      duration:8000,
+      style:{
+        background:"green",
+        color:"white",
+        fontWeight:"bolder",
+        fontSize:"17px",
+        padding:"20px"
+      }
+    })
+    console.log(receipt,"receipt")
+    console.log(clamiedTokenId,"clamiedTokenId")
+    console.log(claimedNFT,"claimedNFT")
+
+
+   }).catch((err) =>{ console.log(err)
+    toast("whoops...Something went wrong!",{
+      style:{
+        background:"red",
+        color:"white",
+        fontWeight:"bolder",
+        fontSize:"17px",
+        padding:"20px"
+      }
+    })
+  }).finally(() => {
+    setloading(false)
+    toast.dismiss(notification)
+  });
+
+  }
+  const connectWithMetamask =useMetamask();
     
     const address=useAddress();
-    console.log(address)
+    
     const disconnect=useDisconnect();
   return (
     <div className="flex h-screen flex-col 
     lg:grid lg:grid-cols-10">
+      <Toaster position='bottom-center'/>
       <div className="bg-gradient-to-br
        from-cyan-800 to-rose-500 lg:col-span-4">
         <div className="flex flex-col items-center
@@ -55,10 +134,26 @@ function NFTDropPage({collection}:Props) {
         <img className='w-80 object-cover pb-10 lg:h-40' 
         src="https://links.papareact.com/bdy"alt=""/>
        <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold">The HymnBharat Ape coding Club | NFT Drop</h1>
-       <p className=' pt-2 text-xl text-green-500'>20/98 NFT's channel</p>
+       {loading ? (
+         <p className=' pt-2 text-xl text-green-500 animate-bounce'>Loading count....</p>
+       ):(
+        <p className=' pt-2 text-xl text-green-500'>{clamiedsuplly}/{totalsuplly?.toString()} NFT's channel</p>
+       )
+      }
+      {loading && <img className='w-80 object-contain h-40' 
+        src="https://miro.medium.com/max/1400/1*CsJ05WEGfunYMLGfsT2sXA.gif"alt=""/>}
+      
        </div>
-       <button className="h-16  mt-10 w-full bg-red-600 rounded-full font-bold text-white">
-        Mint NFT (0.01 ETH)
+       <button onClick={minNFT} disabled={loading || clamiedsuplly === totalsuplly?.toNumber() || !address} className="h-16  mt-10 w-full  bg-red-600 rounded-full font-bold text-white disabled:bg-400">
+        {loading ? (
+          <>Loading</>
+        ):clamiedsuplly === totalsuplly?.toNumber() ? (
+          <>Sold out</>
+
+        ):!address ? (
+          <>Sign in to mint</>
+        ):(<span className="font-bold">Mint NFT ({priceInEth}) ETH</span>) }
+        
        </button>
        
       </div>
@@ -98,7 +193,7 @@ export const getServerSideProps:GetServerSideProps=async ({params}) =>{
   const collection = await sanityClient.fetch(query,{
     id:params?.id,
   });
-  console.log(collection,"900")
+  
 if(!collection){
   return {
     notFound:true
